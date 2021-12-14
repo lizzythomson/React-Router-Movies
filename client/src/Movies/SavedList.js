3f0dedff9f7cdec1888cd6ff17fada050f5d6dcb
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2021-12-13 19:53:12 -0700

    Stretch Goal: added NavLinks

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function SavedList(props) {
   return (
-    <Link to="/">
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {props.list.map((movie) => (
-          <span key={movie.id} className="saved-movie">
-            {movie.title}
-          </span>
-        ))}
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map((movie) => (
+        <NavLink
+          to={`/movies/${movie.id}`}
+          key={movie.id}
+          className="saved-movie"
+        >
+          {movie.title}
+        </NavLink>
+      ))}
+      <Link to="/">
         <div className="home-button">Home</div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
